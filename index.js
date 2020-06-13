a869baac792283a055cecc4b0d95d1106a4a9bc0
penny <925580783@qq.com>
2020-06-13 16:38:31 +0800

    Complete the job as required by 'index.js'.

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
 // TODO write your code here...
+console.log(name.toLocaleUpperCase());
 
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
+var sentenceArr = sentence.split(' ');
+var result = '';
+for(var i in sentenceArr){
+    result += sentenceArr[i].substring(0,1).toLocaleUpperCase() + sentenceArr[i].substring(1) + ' ';
+}
+console.log(result);
+
 
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+var value = money.substring(1);
+console.log(value);
